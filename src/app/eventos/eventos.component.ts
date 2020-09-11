17d3068166f2aa4c2dc8f55536f21ce20a0cdddc
João Camelo <1140295@isep.ipp.pt>
2020-09-11 21:15:27 +0100

    added table of events

--- src/app/eventos/eventos.component.ts
+++ src/app/eventos/eventos.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { CalendarComponent } from 'ng-fullcalendar';
-// import { Options } from 'fullcalendar';
 
 @Component({
   selector: 'app-eventos',
@@ -8,9 +6,6 @@ import { CalendarComponent } from 'ng-fullcalendar';
   styleUrls: ['./eventos.component.css', '../styles/cardcontent.css']
 })
 export class EventosComponent implements OnInit {
-  /*calendarOptions: Options;
- displayEvent: any;
-  @ViewChild(CalendarComponent) ucCalendar: CalendarComponent;*/
   public calendar: any[] = [
     {
       name: 'Dezembro',
@@ -56,48 +51,7 @@ export class EventosComponent implements OnInit {
     }
   ];
   ngOnInit() {
-     /*this.calendarOptions = {
-        editable: false,
-        eventLimit: false,
-        header: {
-          left: 'prev,next today',
-          center: 'title',
-          right: 'month,agendaWeek,agendaDay,listMonth'
-        },
-        events: null
-      };*/
+
   }
 
-  /*clickButton(model: any) {
-    this.displayEvent = model;
-  }
-  eventClick(model: any) {
-    model = {
-      event: {
-        id: model.event.id,
-        start: model.event.start,
-        end: model.event.end,
-        title: model.event.title,
-        allDay: model.event.allDay
-        // other params
-      },
-      duration: {}
-    };
-    this.displayEvent = model;
-  }
-  updateEvent(model: any) {
-    model = {
-      event: {
-        id: model.event.id,
-        start: model.event.start,
-        end: model.event.end,
-        title: model.event.title
-        // other params
-      },
-      duration: {
-        _data: model.duration._data
-      }
-    };
-    this.displayEvent = model;
-  }*/
 }
