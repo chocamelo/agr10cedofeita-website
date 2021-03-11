f245fbe704a50673f1a7c4d301537f58458ba02c
João Camelo <1140295@isep.ipp.pt>
2021-03-11 20:12:03 +0000

    package
    m
    
    pakig
    
    p
    
    m
    
    m
    
    m
    
    m
    
    diff
    
    m
    
    m
    
    it works

--- src/app/galeria/album/album.component.ts
+++ src/app/galeria/album/album.component.ts
@@ -4,17 +4,20 @@ import { DOCUMENT } from '@angular/common';
 @Component({
   selector: 'app-album',
   templateUrl: './album.component.html',
-  styleUrls: ['./album.component.css', '../../styles/cardcontent.css']
+  styleUrls: [
+    './album.component.css',
+    '../../styles/cardcontent.css'
+  ]
 })
 export class AlbumComponent implements OnInit {
 
   albumName: string;
 
-  constructor(@Inject(DOCUMENT) private document: Document) { 
+  constructor(@Inject(DOCUMENT) private document: Document) {
     console.log(this.document.location.href);
     this.albumName = this.document.location.href.match('(?<=galeria/).*$')[0];
     console.log(this.albumName);
-}
+  }
 
   ngOnInit() {
   }
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -56,6 +56,9 @@
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
+const w: any = window;
+// Fix "missing __importDefault", as per https://github.com/angular/angular/issues/32215#issuecomment-543459862
+w.__importDefault = (w && w.__importDefault) || function (mod) { return (mod && mod.__esModule) ? mod : { "default": mod }; }
 
 
 /***************************************************************************************************
